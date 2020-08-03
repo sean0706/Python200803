882f17ab83aecca63707c0cfd9330ed5bd928dc2
sean0706 <69132004+sean0706@users.noreply.github.com>
2020-08-03 11:14:24 +0800

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:12 2020
+
+@author: AE401
+"""
+
+print("I'm Eric")
+print('He is "cool"')
+
---
+++ Day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:34:47 2020
+
+@author: AE401
+"""
+
+name=input("What's you name")
+print("Hi"+name)
